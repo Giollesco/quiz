a3cf4e5fd0ccfcc92410e4b10963d791b6731726
Giollesco <ivandjolo@gmail.com>
2024-01-21 22:27:14 +0100

    task(game): Switching questions added, randomization

--- app/src/main/java/com/example/quiz/game/Game.java
+++ app/src/main/java/com/example/quiz/game/Game.java
@@ -13,20 +13,31 @@ import android.widget.TextView;
 
 import com.example.quiz.R;
 import com.example.quiz.models.GameButtonState;
+import com.example.quiz.models.GameQuestion;
 import com.example.quiz.models.GameState;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class Game extends AppCompatActivity {
     private DatabaseReference db;
-    private ConfirmationSheet confirmationSheetFragment;
+    private GameQuestion[] questions;
+    private TextView questionText;
+    private TextView currentQuestionIndexText;
+    private TextView totalNumOfQuestionsText;
     private Button[] allButtons;
     private Button selectedButton;
     private GameState gameState = GameState.IDLE;
-    private Number currentQuestion = 0;
+    private int currentQuestionIndex = 0;
+    private List<GameQuestion> shuffledQuestions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +45,12 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         this.db = FirebaseDatabase.getInstance().getReference().child("questions");
-        confirmationSheetFragment = new ConfirmationSheet();
+        ConfirmationSheet confirmationSheetFragment = new ConfirmationSheet();
         ConstraintLayout backButton = findViewById(R.id.game_back_button);
 
-        // Fetching questions
+        // Fetching questions and setting up
         fetchQuestions();
+        showNextQuestion();
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +58,9 @@ public class Game extends AppCompatActivity {
             }
         });
 
+        questionText = findViewById(R.id.game_question);
+        currentQuestionIndexText = findViewById(R.id.current_question_index);
+        totalNumOfQuestionsText = findViewById(R.id.total_num_of_questions);
         Button button_a = findViewById(R.id.game_option_a);
         Button button_b = findViewById(R.id.game_option_b);
         Button button_c = findViewById(R.id.game_option_c);
@@ -80,32 +95,34 @@ public class Game extends AppCompatActivity {
     }
 
     public void onConfirmationSheetButtonClick() {
-        if(gameState == GameState.PENDING_CONFIRMATION){
+        if (gameState == GameState.PENDING_CONFIRMATION) {
             changeButtonState(selectedButton, GameButtonState.CORRECT);
             updateTextInFragment("Sljedeće pitanje");
             gameState = GameState.PENDING_NEXT_QUESTION;
-        }
-        else if (gameState == GameState.PENDING_NEXT_QUESTION){
-            changeButtonState(selectedButton, GameButtonState.INACTIVE);
-            updateTextInFragment("Sljedeće pitanje");
-            closeConfirmationSheetFragment();
-            gameState = GameState.IDLE;
-        }
-        else{
+        } else if (gameState == GameState.PENDING_NEXT_QUESTION) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    showNextQuestion();
+                    changeButtonState(selectedButton, GameButtonState.INACTIVE);
+                    updateTextInFragment("Jeste li sigurni?");
+                    closeConfirmationSheetFragment();
+                    gameState = GameState.IDLE;
+                }
+            });
+        } else {
             gameState = GameState.PENDING_CONFIRMATION;
         }
     }
 
-    // Method to update the text in the fragment
     public void updateTextInFragment(String newText) {
         TextView text = findViewById(R.id.fragment_text);
-        if(text != null){
+        if (text != null) {
             text.setText(newText);
         }
     }
 
     private void showConfirmationSheetFragment() {
-        // Check if the fragment is already visible
         if (getSupportFragmentManager().findFragmentByTag(ConfirmationSheet.class.getSimpleName()) == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.setCustomAnimations(R.anim.slide_up, 0, 0, 0);
@@ -123,43 +140,85 @@ public class Game extends AppCompatActivity {
         }
     }
 
-
     public void changeButtonState(Button button, GameButtonState state) {
         switch (state) {
             case INACTIVE:
-                // Set background color and font color for INACTIVE state
                 button.setBackgroundResource(R.drawable.game_button_inactive_style);
                 button.setTextColor(getResources().getColor(R.color.light_gray));
                 break;
             case IDLE:
-                // Set background color and font color for IDLE state
                 button.setBackgroundResource(R.drawable.game_button_idle_style);
                 button.setTextColor(getResources().getColor(R.color.black));
                 break;
             case CORRECT:
-                // Set background color and font color for CORRECT state
                 button.setBackgroundResource(R.drawable.game_button_correct_style);
                 button.setTextColor(getResources().getColor(R.color.black));
                 break;
             case INCORRECT:
-                // Set background color and font color for INCORRECT state
                 button.setBackgroundResource(R.drawable.game_button_incorrect_style);
                 button.setTextColor(getResources().getColor(R.color.black));
                 break;
         }
     }
+    private void shuffleQuestions() {
+        if (questions != null && questions.length > 0) {
+            shuffledQuestions = Arrays.asList(questions);
+            Collections.shuffle(shuffledQuestions);
+        }
+    }
 
-    public void fetchQuestions(){
+    private void showNextQuestion() {
+        if (shuffledQuestions != null && !shuffledQuestions.isEmpty()) {
+            if (currentQuestionIndex < shuffledQuestions.size()) {
+                GameQuestion nextQuestion = shuffledQuestions.get(currentQuestionIndex);
+                currentQuestionIndex++;
+                questionText.setText(nextQuestion.getQuestion());
+                currentQuestionIndexText.setText(String.valueOf(currentQuestionIndex));
+            } else {
+                // Restart the loop from the beginning
+                currentQuestionIndex = 0;
+                shuffleQuestions();
+                // Recursive call to display the first question after shuffling
+                showNextQuestion();
+            }
+        } else {
+            Log.e("Game", "Questions not shuffled or empty");
+        }
+    }
+
+
+    public void fetchQuestions() {
         db.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
+                if (task.isSuccessful()) {
+                    DataSnapshot dataSnapshot = task.getResult();
+                    if (dataSnapshot.exists()) {
+                        GenericTypeIndicator<List<GameQuestion>> typeIndicator = new GenericTypeIndicator<List<GameQuestion>>() {};
+                        List<GameQuestion> questionsList = dataSnapshot.getValue(typeIndicator);
+
+                        if (questionsList != null) {
+                            questions = questionsList.toArray(new GameQuestion[0]);
+                            // Shuffle the questions
+                            shuffleQuestions();
+                            // Initial setup
+                            if (questions.length > 0) {
+                                totalNumOfQuestionsText.setText(String.valueOf(questions.length));
+                                currentQuestionIndex = 0;
+                                showNextQuestion();
+                            }
+                        } else {
+                            Log.e("firebase", "Failed to parse questions list");
+                        }
+                    } else {
+                        Log.e("firebase", "No data in the 'questions' node");
+                    }
+                } else {
                     Log.e("firebase", "Error getting data", task.getException());
-                }
-                else {
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
                 }
             }
         });
     }
+
+
 }
